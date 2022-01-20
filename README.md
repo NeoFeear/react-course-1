a8581dff20d2a777a95ff07a9aca50cdb9ccfb41
Florian Martin <neofeear@gmail.com>
2022-01-20 11:59:01 +0100

    Fin de cours du 20/01

--- README.md
+++ README.md
@@ -15,12 +15,12 @@ yarn start
 
 ## Goals
 You have to create a todo list with these features:
-- Create new todo with title / description / priority / assigned to
-- Manage status (completed or not)
-- Create new list
-- Add filters
-- Edit todo
-- Move todo
-- Drag and drop todo (using [beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd))
-- List virtualization (using [react-window](https://github.com/bvaughn/react-window))
-- Use React context
+- [] Create new todo with title / description / priority / assigned to
+- [] Manage status (completed or not)
+- [] Create new list
+- [] Add filters
+- [] Edit todo
+- [] Move todo
+- [] Drag and drop todo (using [beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd))
+- [] List virtualization (using [react-window](https://github.com/bvaughn/react-window))
+- [] Use React context
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import _ from 'lodash';
+import { v4 } from 'uuid';
 
-export default () => <div className="App">React Course 1</div>;
+const item = [
+    {
+        id: v4(),
+        name: 'Learn React'
+    },
+    {
+        id: v4(),
+        name: 'Learn TypeScript'
+    }
+]
+
+function App() {
+    const [state, setState] = useState({
+        "todo": {
+            title: "Todo",
+            items: [item[0]]
+        },
+        "inProgress": {
+            title: "In Progress",
+            items: [item[1]]
+        },
+        "done": {
+            title: "Completed",
+            items: []
+        }
+    });
+
+    const handleDragEnd = ({destination, source}: {destination: number, source: number}) => {
+        if (!destination) {
+            console.log('destination is null');
+            return
+        }
+
+        if (destination.index === source.index && destination.droppableId === source.droppableId) {
+            console.log('destination is same as source');
+        }
+
+        const itemCopy = {...state[source.droppableId].items[source.index]};
+
+        setState(prevState => {
+            prevState = {...prevState},
+            prevState[source.droppableId].items.splice(source.index, 1);
+            prevState[destination.droppableId].items.splice(destination.index, 0, itemCopy);
+            return prevState;
+        });
+
+    }
+
+    return (
+        <div className="App">
+
+            <DragDropContext onDragEnd={handleDragEnd}>
+                {_.map(state, (data, key) => {
+                    return (
+                        <div key={key} className={"column"}>
+                            <h2>{data.title}</h2>
+                            
+                            <Droppable droppableId={key}>
+                                {(provided) => {
+                                    return(
+                                        <div ref={provided.innerRef}{...provided.droppableProps} className={"droppable-col"}>
+                                            {data.items.map((el, index) => {
+                                                return (
+                                                    <Draggable key={el.id} index={index} draggableId={el.id}>
+                                                        {(provided) => {
+                                                            return (
+                                                                <div ref={provided.innerRef}{...provided.draggableProps} className={"item"}>
+                                                                    <span {...provided.dragHandleProps} className={"drag-handle"}>DRAG</span>
+                                                                    &nbsp;-&nbsp; 
+                                                                    {el.name}
+                                                                </div>
+                                                            )
+                                                        }}
+                                                    </Draggable>
+                                                )
+                                            })}
+                                            {provided.placeholder}
+                                        </div>
+                                    )
+                                }}
+                            </Droppable>
+                        </div>
+                    )
+                })}
+            </DragDropContext>
+
+        </div>
+    );
+}
+
+export default App;
